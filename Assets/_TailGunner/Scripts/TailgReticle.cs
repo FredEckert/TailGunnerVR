2f6f029ab75a2d4d6ae6a72131e0e946c7c7e89b
Fred Eckert <fredeckert@hotmail.com>
2019-01-17 22:48:07 -0500

    Add raycast distance and scaling to targeting reticle

--- Assets/_TailGunner/Scripts/TailgReticle.cs
+++ Assets/_TailGunner/Scripts/TailgReticle.cs
@@ -7,6 +7,7 @@ public class TailgReticle : MonoBehaviour {
 
     public static VectorLine Reticle;
     public float reticleScale = 0.001f;
+    private float defaultPosZ;
 
     public static TailgReticle use;
 
@@ -15,53 +16,52 @@ public class TailgReticle : MonoBehaviour {
         TailgReticle.use = this;
     }
 
-    public void StartUp() {
-
+    public void StartUp()
+    {
         //Set up target reticle lines and colors...
-        //
-        //set reticle to one meter in front of player
         //set reticle up 1.7 meters to match player's offset from zero
-        this.gameObject.transform.position = new Vector3(0, 1.7f, 1);
+        //set reticle distance to just inside far clip plane
+        defaultPosZ = 1860f;
+        this.gameObject.transform.position = new Vector3(0, 1.7f, defaultPosZ);
         this.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
         //scale reticle
         for (int index = 0; index < LineData.use.reticlePoints.Count; index++)
         {
             Vector3 point = LineData.use.reticlePoints[index];
-            //point.x = point.x + 30f;
-            //point.y = point.y + 20f;
-            //point.y = point.y + (1 / this.reticleScale);
             point = point * this.reticleScale;
             LineData.use.reticlePoints[index] = point;
         }
-
+        //Create reticle
         Reticle = new VectorLine(gameObject.name, LineData.use.reticlePoints, Manager.use.lineWidth);
         Reticle.material = Manager.use.lineMaterial;
         Reticle.texture = Manager.use.lineTexture;
         Reticle.color = Manager.use.colorNormal;
         Reticle.capLength = Manager.use.capLength;
-        
         // Make VectorManager lines be drawn in the scene instead of as an overlay
         VectorManager.useDraw3D = true;
         // Make this transform have the vector line object that's defined above
         VectorManager.ObjectSetup(gameObject, Reticle, Visibility.Dynamic, Brightness.None);
-
-        //TailgUI.Reticle = this.MakeLine("Reticle", LineData.use.reticlePoints);
-        //TailgUI.Reticle.color = Manager.use.MultiplyColor(Manager.use.colorNormal, 0.75f);
-        //TailgUI.Reticle.color = Manager.use.colorNormal;
-        //this.SetReticleLines();
-        //TailgUI.Reticle.active = false;
-
-        //public virtual void SetReticleLines()
-        //{
-        //    //Manager.use.ScalePointsToScreen(TailgUI.targetReticleNormal.points2);
-        //    //Manager.use.ScalePointsToScreen(TailgUI.targetReticleActive.points2);
-        //    TailgUI.Reticle.Draw3D();
-        //}
-
     }
 
     // Update is called once per frame
-    void Update () {
-
+    void Update()
+    {
+        Transform camera = Camera.main.transform;
+        Ray ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        RaycastHit hit;
+        float distance;
+        if (Physics.Raycast(ray, out hit))
+        {
+            distance = hit.distance;
+            Reticle.color = Manager.use.colorIntense;
+        }
+        else
+        {
+            distance = defaultPosZ;
+            Reticle.color = Manager.use.colorNormal;
+        }
+        transform.localPosition = new Vector3(0, 0, distance);
+        transform.localScale = Vector3.one * distance;
     }
+
 }
